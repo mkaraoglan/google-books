1e030a3901cd84fb36ab0f8d447162e2f9a92c9f
Mucahit <mucahitkaraoglan@gmail.com>
2022-03-06 15:56:24 +0300

    api init

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+var corOptions = {
+  origin: 'https://localhost:8081',
+};
+
+// middleware
+app.use(cors(corOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+// testing api
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello Fullstack Developer' });
+});
+
+// port
+
+const PORT = process.env.PORT || 8080;
+
+// server
+app.listen(PORT, () => {
+  console.log(`server is running port ${PORT}`);
+});
